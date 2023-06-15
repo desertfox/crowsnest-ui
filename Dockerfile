902daac64ede11ecc06a300906f5a6deaf997a85
Rummy <cerealbh@gmail.com>
2023-06-15 12:22:27 -0500

    Build Variable
    Added code to make API_URL configurable through webpack build.

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
 FROM node:19 AS build
 
+ARG API_URL=localhost:8080
+
+ENV API_URL=${API_URL}
+
 WORKDIR /tmp
 
 COPY . .
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,42 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 const path = require("path");
 
-module.exports = {
-  mode: 'development',
-  entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "crow.bundle.js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-			presets: ['@babel/preset-env', '@babel/preset-react'],
+module.exports = (env) => {
+  const config = {
+    mode: 'development',
+    entry: "./src/index.js",
+    output: {
+      path: path.resolve(__dirname, "build"),
+      filename: "crow.bundle.js",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+        presets: ['@babel/preset-env', '@babel/preset-react'],
+            },
           },
         },
-      },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'src/index.html',
+      }), 
+      new webpack.DefinePlugin({
+        API_URL: JSON.stringify(env.APP_URL),
+      }),
     ],
-  },
-  plugins: [new HtmlWebpackPlugin({
-    template: 'src/index.html',
-  })],
+  };
+
+  return config;
 };
